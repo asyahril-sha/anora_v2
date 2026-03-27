8d2d248aef4fbbcfb6320575570c53aa9d595390
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 04:07:52 +0800

    Update __init__.py

--- roles/__init__.py
+++ roles/__init__.py
@@ -1,26 +1,22 @@
 # anora/roles/__init__.py
-
-"""
-ANORA-V2 Roles Package
-Semua role dengan akses penuh sesuai level, sama seperti Nova.
-"""
-
-from .therapist_role import TherapistRole, get_random_therapist
-from .pelacur_role import PelacurRole
 from .base_role import BaseRole
 from .ipar_role import IparRole
 from .teman_kantor_role import TemanKantorRole
+from .pelakor_role import PelakorRole
 from .istri_orang_role import IstriOrangRole
+from .therapist_role import TherapistRole, get_therapist_manager
+from .pelacur_role import PelacurRole
 from .role_manager import RoleManager, get_role_manager
 
 __all__ = [
     'BaseRole',
-    'TherapistRole',
-    'PelacurRole',
     'IparRole',
     'TemanKantorRole',
+    'PelakorRole',
     'IstriOrangRole',
+    'TherapistRole',
+    'PelacurRole',
     'RoleManager',
     'get_role_manager',
-    'get_random_therapist',
+    'get_therapist_manager',
 ]
