418f58900bbc5fe0b4ca3b86f0c9aba5e0eb8422
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 06:13:37 +0800

    Update __init__.py

--- roles/__init__.py
+++ roles/__init__.py
@@ -1,17 +1,16 @@
 # anora/roles/__init__.py
 """
 ANORA Roles Package
-Semua role dengan engine sendiri
 """
 
-from .base import BaseRole
+from .base_role import BaseRole
 from .ipar import IparRole
 from .teman_kantor import TemanKantorRole
 from .pelakor import PelakorRole
 from .istri_orang import IstriOrangRole
 from .therapist_role import TherapistRole, get_therapist_manager
 from .pelacur_role import PelacurRole, get_pelacur_manager
-from .manager import RoleManager, get_role_manager
+from .manager import RoleManager, get_role_manager, normalize_role_id
 
 __all__ = [
     'BaseRole',
@@ -25,4 +24,5 @@ __all__ = [
     'get_role_manager',
     'get_therapist_manager',
     'get_pelacur_manager',
+    'normalize_role_id',
 ]
