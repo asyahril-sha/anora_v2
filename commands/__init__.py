85547eb9dc1f64b3b32945d5d701fb3c213767bd
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 09:23:26 +0800

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,8 +1,8 @@
 # anora_v2/commands/__init__.py
-from .general import register_general_commands
-from .role import register_role_commands
-from .therapist import register_therapist_commands
-from .pelacur import register_pelacur_commands
+from commands.general import register_general_commands
+from commands.role import register_role_commands
+from commands.therapist import register_therapist_commands
+from commands.pelacur import register_pelacur_commands
 
 __all__ = [
     'register_general_commands',
