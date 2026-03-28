27b7cf3dbb114e767befda4a7c286804f0671c5d
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 09:02:57 +0800

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,11 @@
+# anora_v2/handlers/__init__.py
+"""
+ANORA-V2 Handlers Package
+"""
+
+from .message import message_handler, set_anora_available
+
+__all__ = [
+    'message_handler',
+    'set_anora_available',
+]
